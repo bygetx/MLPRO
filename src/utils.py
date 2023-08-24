75f4f71c62b2976a8964e69efc3f9055b66d2578
Aymen Mehrez <mehrezaymen0@gmail.com>
2023-08-24 03:24:19 +0100

    prediction pipeline

--- src/utils.py
+++ src/utils.py
@@ -45,3 +45,11 @@ def evaluate_models(X_train , y_train , X_test , y_test , models:dict, param):
 
     except Exception as e:
         raise CustomException(e,sys)
+    
+
+def load_object(file_path):
+    try:
+        with open(file_path,"rb") as file:
+            return dill.load(file)
+    except Exception as e:
+        raise CustomException(e,sys)
